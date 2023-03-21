21bffb6890dfb602ee7444a1c076d91ef8304f49
Anderson Franciscato <andersonwsf@gmail.com>
2023-03-20 21:22:49 -0300

    comit inicial

---
+++ just-script.js
@@ -0,0 +1,8 @@
+function simbut() {
+    alert("Graças a Deus náo passei vergonha")
+    window.open("https://www.youtube.com/watch?v=X634wj6DQDI&ab_channel=EuronewsemPortuguês")
+}
+
+function nobut() {
+    alert("Escolha errada tenta outra vez")
+}
